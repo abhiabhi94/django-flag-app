e6a1324d88c2066fa6fb64adb733147d991bac6b
abhiabhi94 <13880786+abhiabhi94@users.noreply.github.com>
2020-11-27 13:37:48 +0530

    :white_check_mark: Add test for the case when drf is not installed
    - cover the case when an exception is thrown and rest framework is not installed.

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,3 +1,6 @@
+import sys
+from unittest.mock import patch
+
 from rest_framework import status
 
 from flag.exceptions import FlagBadRequest
@@ -5,13 +8,25 @@ from tests.base import TestCase
 
 
 class FlagExceptionTest(TestCase):
+    _default_detail = 'Bad Request'
+
     def test_can_create_custom_error_without_params(self):
         exception = FlagBadRequest()
         self.assertEqual(exception.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(exception.detail, "Bad Request")
+        self.assertEqual(exception.detail, self._default_detail)
 
     def test_create_custom_error_with_params(self):
         detail = 'not found'
         exception = FlagBadRequest(detail=detail, status_code=404)
         self.assertEqual(exception.status_code, status.HTTP_404_NOT_FOUND)
         self.assertEqual(exception.detail, detail)
+
+    def test_create_custom_error_without_drf_installed(self):
+        with patch.dict(sys.modules, {'rest_framework.exceptions': None}):
+            from importlib import reload
+            reload(sys.modules['flag.exceptions'])
+            from flag.exceptions import FlagBadRequest
+            exception = FlagBadRequest()
+
+        self.assertEqual(exception.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(exception.detail, self._default_detail)
