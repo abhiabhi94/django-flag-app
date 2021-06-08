13832d7deeca54685ba4b0b5aa1a753b95df8f8d
abhiabhi94 <13880786+abhiabhi94@users.noreply.github.com>
2021-06-08 19:40:23 +0530

    chore: 🔧 use more inclusive language for placeholders

--- testapp/settings.py
+++ testapp/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dummy'
+SECRET_KEY = 'ThisSecretKeyIsNotSafe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
