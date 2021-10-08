a15de7ef1a3476d43691bc7f1024d90ac3b85939
abhiabhi94 <13880786+abhiabhi94@users.noreply.github.com>
2021-10-09 00:32:01 +0530

    feat: ✨ Confirm support for python 3.10
    - also run workflow actions on python 3.10.

--- flag/conf/__init__.py
+++ flag/conf/__init__.py
@@ -10,11 +10,13 @@ class LazySettings(LazyObject):
         self._wrapped = Settings(flag_settings, django_settings)
 
 
+_django_version = django.VERSION
 DEPRECATED_SETTINGS = {
-    'USE_TZ' if django.VERSION > (4, 0) else None,
-    'PASSWORD_RESET_TIMEOUT_DAYS' if django.VERSION > (3, 0) else None,
-    'DEFAULT_CONTENT_TYPE' if django.VERSION > (2, 2) else None,
-    'FILE_CHARSET' if django.VERSION > (2, 2) else None,
+    'USE_TZ' if _django_version > (4, 0) else None,
+    'PASSWORD_RESET_TIMEOUT_DAYS' if _django_version > (3, 0) else None,
+    'DEFAULT_CONTENT_TYPE' if _django_version > (2, 2) else None,
+    'FILE_CHARSET' if _django_version > (2, 2) else None,
+    'USE_L10N' if _django_version > (4, 0) else None,
 }
 
 
